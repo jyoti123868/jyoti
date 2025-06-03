02c7b3c259bc1137715e7e6bec18417f9dbf14db
jyoti123868 <jjyotirathee@gmail.com>
2025-06-03 19:57:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("Hello o am working on streamlit")
+name=st.text_input("enter your name")
+st.title("my first streamlit app")
+st.write("Hello, creating first streamlit app")
